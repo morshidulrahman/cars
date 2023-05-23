bb9999f9914f554d21bca6e7464d339541fab3d0
morshidulrahman <boydanger416@gmail.com>
2023-05-23 16:22:14 +0600

    about-done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,23 +2,34 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <img
-            src="/images/about_us/person.jpg"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
+    <div className="flex md:justify-between flex-wrap gap-4 py-20 justify-center">
+      <div className="relative md:w-[48%] w-full ">
+        <img
+          src="/images/about_us/person.jpg"
+          className="w-[350px] h-[300px] shadow-2xl object-cover rounded-md"
+        />
+        <img
+          src="/images/about_us/parts.jpg"
+          className="w-[300px] h-[300px] bg-white p-2 absolute right-28 -bottom-10  object-cover rounded-md"
+        />
+      </div>
+      <div className="space-y-3 md:w-[48%] w-full">
+        <h4 className="font-bold text-[#FF3811] capitalize">about us</h4>
+        <h1 className="font-bold text-3xl w-1/2">
+          We are qualified & of experience in this field
+        </h1>
+        <p>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+        <button className="border border-[#FF3811] px-4 py-3 rounded-md text-white bg-[#FF3811] font-semibold">
+          Get more info
+        </button>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./components/routes/route.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-7xl mx-auto ">
+    <div className="max-w-7xl mx-auto container px-4 md:px-6">
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>
